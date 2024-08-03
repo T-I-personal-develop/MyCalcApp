42dd3dbaa8d9e4d241194635fb6af5aa30301b3e
t-icke <irishiotsukasa@gmail.com>
2024-08-03 21:18:16 +0900

    計算用モデルクラス追加

---
+++ MyCalcApp/Models/Data/CalcData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MyCalcApp.Categories.MyCategory;
+
+namespace MyCalcApp.Models.Data
+{
+    /// <summary>
+    /// 計算用モデルクラス(操作データ)
+    /// </summary>
+    public class CalcData
+    {
+        public decimal PrevValue { get; set; } = 0;   //前の項
+        public EnumCommandType2 Operation { get; set; } = EnumCommandType2.Other;  //演算子
+        public decimal NextValue { get; set; } = 0;   //次の項
+
+    }
+}
