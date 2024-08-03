768db1d361c92a2f79682a928b06f4b600887fa0
t-icke <irishiotsukasa@gmail.com>
2024-08-03 19:36:22 +0900

    カテゴリクラス追加

---
+++ MyCalcApp/Categories/MyCategory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalcApp.Categories
+{
+    /// <summary>
+    /// カテゴリクラス
+    /// </summary>
+    public class MyCategory
+    {
+        /// <summary>
+        /// コマンドタイプ大項目
+        /// </summary>
+        public enum EnumCommandType
+        {
+            Num,        //数値
+            Add,    //＋
+            Substract,//―
+            Multiply,//×
+            Divide,//÷
+            ClearAll,   //全てクリア
+            ClearEntry, //入力クリア
+            Equal,      //計算
+        }
+    
+    }
+}
