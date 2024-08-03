adc6656e1d3f284d20c493e61d45a51e5d66ea83
t-icke <irishiotsukasa@gmail.com>
2024-08-03 19:24:21 +0900

    フォルダ追加 2重起動防止機能追加

--- MyCalcApp/Program.cs
+++ MyCalcApp/Program.cs
@@ -1,17 +1,48 @@
+using Microsoft.VisualBasic.Logging;
+using System.Reflection;
+
 namespace MyCalcApp
 {
     internal static class Program
     {
+        private static Mutex? mutex;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            bool createdNew;
+            mutex = new Mutex(true, assemblyName, out createdNew);
+
+            try
+            {
+
+                // 二重起動をチェック
+                if (!createdNew)
+                {
+                    return;
+                }
+
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                // ミューテックスが取得されている場合にのみリリース
+                if (createdNew)
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
         }
+
     }
 }
